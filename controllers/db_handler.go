7dd2ef96adaac5598b518d0c344509d9b58844fa
KatherinYuliana <katherin.shibuki@gmail.com>
2024-03-21 23:16:13 +0700

    edit

--- controllers/db_handler.go
+++ controllers/db_handler.go
@@ -2,20 +2,16 @@ package controllers
 
 import (
 	"database/sql"
-	//_ "github.com/mattn/go-sqlite3"
+	
 
 	"github.com/labstack/echo/v4"
 )
 
 func Connect() *sql.DB{
 	e := echo.New()
-	//db, err := sql.Open("sqlite3", "lat_pbp_2")
 	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/lat_pbp_2?parseTime=true&loc=Asia%2FJakarta")
 	if err != nil {
-		// log.Fatal(err)
-		//return nil, err
 		e.Logger.Fatal(err)
 	}
 	return db
-	//defer db.Close()
 }
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"Framework_API/controllers"
 	//"database/sql"
-	"fmt"
-	"log"
-	"net/http"
+	// "fmt"
+	// "log"
+	// "net/http"
 
 	//"database/sql"
 	_ "github.com/go-sql-driver/mysql"
@@ -14,24 +14,14 @@ import (
 
 func main() {
 	db := controllers.Connect()
-	// db, err := controllers.Connect()
-    // if err != nil {
-    //     log.Fatal(err)
-    // }
     defer db.Close()
-	// defer db.Close()
+	
 	e := echo.New()
 
 	e.POST("/users", controllers.InsertUsers(db))
-
-	// e.POST("/users", controllers.InsertUsers())
 	e.GET("/users", controllers.GetAllUsers(db))
 	e.PUT("/users/:id", controllers.UpdateUsers(db))
 	e.DELETE("/users/:id", controllers.DeleteUsers(db))
 
-	// e.Logger.Fatal(e.Start(":8888"))
 	e.Start(":8888")
-	http.Handle("/", e)
-	fmt.Println("Connected to port 8888")
-	log.Println("Connected to port 8888")
 }
